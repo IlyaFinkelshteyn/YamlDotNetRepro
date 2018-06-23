8cbc3654f29035209d3482de57d3ab123567c836
ilyaf f <ilyaf@appveyor.com>
2018-06-22 18:48:20 -0700

    first commit

--- YamlDotNetRepro/Program.cs
+++ YamlDotNetRepro/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using YamlDotNet.RepresentationModel;
 
 namespace YamlDotNetRepro
 {
@@ -10,6 +8,29 @@ namespace YamlDotNetRepro
     {
         static void Main(string[] args)
         {
+            var yaml = new YamlStream();
+            var rootNode = new YamlMappingNode();
+            for (int i = 0; i < 5; i++)
+            {
+                var random = new Random(i).Next(100, 2000).ToString();
+
+                // repro with this line
+                rootNode.Add(random, random);
+
+                // NO repro with this line instead of the one above           
+                //rootNode.Add("key" + i, "value" + i);
+            }
+            yaml.Documents.Add(new YamlDocument(rootNode));
+            var writer = new StringWriter();
+            yaml.Save(writer);
+            Console.WriteLine(writer.ToString().Replace("\r\n...\r\n", ""));
+
+            //Output look like this
+            // &1944107265 1479: 1479
+            // &1075620271 572: 572
+            // &1182217550 1565: 1565
+            // &2008746139 657: 657
+            // &1222564548 1650: 1650
         }
     }
 }
